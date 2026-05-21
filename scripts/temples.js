bd91f7058b123832c9a30796d6e76cf1fed30295
buyan-pro <buyannemekh33@gmail.com>
2026-05-21 21:41:35 +0800

    W01 Assignment Picture Album commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -6,3 +6,20 @@ document.getElementById("currentyear").textContent = currentYear;
 
 // Insert last modified date
 document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
+
+// Hamburger menu
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+
+    // Toggle navigation menu
+    navigation.classList.toggle("open");
+
+    // Change hamburger icon
+    if (navigation.classList.contains("open")) {
+        menuButton.textContent = "✖";
+    } else {
+        menuButton.textContent = "☰";
+    }
+});
